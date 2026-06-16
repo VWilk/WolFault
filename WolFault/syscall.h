120624015add5126587dc4855c387f9be01b2bf3
baloxeboloxe-ctrl <baloxeboloxe@gmail.com>
2026-06-16 09:13:37 -0700

    Update syscall.h

--- WolFault/syscall.h
+++ WolFault/syscall.h
@@ -63,7 +63,7 @@ namespace scemaster {
 		
 
 		/* Relies on GetSSN and NtReadVirtualMemory to read memory, stores in buffer. */
-		[[noinline]] LPVOID ReadMem(HANDLE hProcess, PVOID baseAddr, SIZE_T size) {
+		[[noinline]] LPVOID ReadMem(HANDLE hProcess, PVOID baseAddr, SIZE_T size) { /*[[noinline]] is not needed here but it doesn't worth anything to keep it ig */
 			LPVOID buf = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
 			if (!buf) return nullptr;
 
